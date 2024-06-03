79567b31bd9b0bab11d83d667d19d4f608e47805
Никита Стамбул <crm.30109@dz.com>
2024-06-03 01:19:30 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nikita Stambul on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
